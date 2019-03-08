153f15ecb1743663872e18c29403840a923c3b37
parkjunghun <go9018@naver.com>
2019-03-09 00:04:54 +0900

    Initial Commit

---
+++ CustomNaviBar_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomNaviBar_ios
+//
+//  Created by 박정훈 on 09/03/2019.
+//  Copyright © 2019 swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //내비게이션 타이틀 초기화
+        self.initTitle()
+    }
+    
+    func initTitle(){
+        
+    }
+
+
+}
+
